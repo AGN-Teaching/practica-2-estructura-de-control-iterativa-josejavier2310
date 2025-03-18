8bcd4b6d50368474716b4b4373cea0dac03b0d9f
josejavier2310 <jose.j.perez@cua.uam.mx>
2025-03-17 18:22:37 -0600

    Update descifrar.py

--- descifrar.py
+++ descifrar.py
@@ -6,20 +6,20 @@ print("codigo desifrar.py")
 # Algoritmo
 def descifrar_mensaje(mensaje_cifrado):
     # Extraer k a partir de las dos primeras letras del mensaje cifrado
-    primera_letra = ord(mensaje_cifrado[0]) - 65
-    segunda_letra = (ord(mensaje_cifrado[1]) - 65) // 2
-    k = primera_letra if primera_letra == segunda_letra else (primera_letra + segunda_letra) // 2
+    primera_letra = ord(mensaje_cifrado[0]) - 65 # Combierte  la primera letra en un numero
+    segunda_letra = (ord(mensaje_cifrado[1]) - 65) // 2 
+    k = primera_letra if primera_letra == segunda_letra else (primera_letra + segunda_letra) // 2 # Si los valores coincide ese es k y si no se calcula el promedio
     mensaje_descifrado = ""
     for caracter in mensaje_cifrado[2:]:  # Omitir los dos primeros caracteres
         if caracter.isalpha():
             ascii_offset = 65 if caracter.isupper() else 97
-            nuevo_caracter = chr((ord(caracter) - ascii_offset - k) % 26 + ascii_offset)
+            nuevo_caracter = chr((ord(caracter) - ascii_offset - k) % 26 + ascii_offset) # Desifra el mensaje usando la formula inversa del cifrado de cesar
             mensaje_descifrado += nuevo_caracter
         else:
             mensaje_descifrado += caracter
-    return mensaje_descifrado, k
+    return mensaje_descifrado, k # Devuelve el mensaje desifrado y k
 # Entrada del usuario
-mensaje_cifrado = input("Introduce el mensaje cifrado: ")
-mensaje_descifrado, k = descifrar_mensaje(mensaje_cifrado)
+mensaje_cifrado = input("Introduce el mensaje cifrado: ") # Solicitamos un mensaje cifrado
+mensaje_descifrado, k = descifrar_mensaje(mensaje_cifrado) # Llama a desifrar_mensaje y muestra el mensaje original
 print("\nMensaje descifrado:", mensaje_descifrado)
 print("Clave de cifrado detectada:", k)illa descifrar.py
